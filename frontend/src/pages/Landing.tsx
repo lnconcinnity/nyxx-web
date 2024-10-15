0a033b361e9cfae7e9c18ec6c2639411a133fd59
RVCDv <101155145+lnconcinnity@users.noreply.github.com>
2024-10-16 00:34:53 +0800

    File updates

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -13,13 +13,29 @@ const Landing = () => {
                         AVA MARIENNE DAYRIT
                     </h1>
                     <div className="w-100 d-flex flex-column justify-content-center align-items-center">
-                        <p className="roboto-thin landing-jumbotron-text fly-horizontal-in-1s-easeOut-inverted">
+                        <p className="roboto-thin landing-jumbotron-text fly-horizontal-in-1s-easeOut-500ms-inverted">
                             Art is like a canvas, and our body is one of them
                         </p>
+                        <div className="d-flex">
+                        {
+                            ['Tattoo Artist', 'Body Piercer', 'Model'].map((text, idx, arr) => (
+                                <>
+                                <span className={`roboto-thin mx-2 fly-vertical-in-500ms-easeOut-${
+                                    `${250*(idx+1)}ms`
+                                }-inverted`}> {text} </span>
+                                {
+                                    arr[idx+1] && (
+                                        <hr style={{width: `15px`, marginTop: '10px'}}/>
+                                    )
+                                }
+                                </>
+                            ))
+                        }
+                        </div>
                     </div>
                 </Col>
                 <Col lg={6} className="vh-100">
-                    <img src={NyxxSelfie1} alt="NyxxSelfie1" style={{width: "100%", height: "100%", objectFit: "contain"}}/>
+                    <img src={NyxxSelfie1} alt="NyxxSelfie1" style={{width: "100%", height: "100%", objectFit: "contain"}} className="fly-horizontal-in-1s-easeOut-0s-normal"/>
                 </Col>
             </Row>
         </Container>
